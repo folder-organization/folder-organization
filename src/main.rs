59a316369d9c111e2d38bc867f36689e1c407d74
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2024-08-15 17:09:17 +0200

    refactor: create get_subfolders function

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,20 @@ use std::env;
 use std::path::Path;
 use ignore::WalkBuilder;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <dossier>", args[0]);
-        return;
-    }
-
-    let dossier = &args[1];
-    let chemin = Path::new(dossier);
-
-    // Vérifie si le chemin est un dossier
-    if !chemin.is_dir() {
-        eprintln!("Le chemin spécifié n'est pas un dossier valide : {}", dossier);
-        return;
-    }
-
-    let mut subfolders = Vec::new();
-    let walker = WalkBuilder::new(chemin)
+fn get_subfolders(folder_path: &Path) -> Vec<String> {
+    let mut subfolders: Vec<String> = Vec::new();
+    
+    let walker = WalkBuilder::new(folder_path)
         .max_depth(Some(1))
         .git_ignore(true)
         .build();
 
     for result in walker {
         if let Ok(entry) = result {
-            let path = entry.path();
-            if path.is_dir() && path != chemin {
-                if let Some(nom) = path.file_name() {
+            let current_path = entry.path();
+            
+            if current_path.is_dir() && current_path != folder_path {
+                if let Some(nom) = current_path.file_name() {                    
                     if let Some(nom_str) = nom.to_str() {
                         subfolders.push(nom_str.to_string());
                     }
@@ -36,7 +23,26 @@ fn main() {
             }
         }
     }
+    
+    subfolders
+}
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <dossier>", args[0]);
+        return;
+    }
+
+    let folder = &args[1];
+    let path = Path::new(folder);
+
+    if !path.is_dir() {
+        eprintln!("Path specified is not a valid folder: {}", folder);
+        return;
+    }
+    
+    let subfolders = get_subfolders(path);    
     let mut result = String::new();
 
     for folder in subfolders {
