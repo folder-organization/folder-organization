dc55bfa0590c51ccdc9ad1185f8361283ad17fc3
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2024-08-15 19:58:39 +0200

    refactor(wip): get_subfolders_path function

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,55 @@
 use ignore::WalkBuilder;
+use std::collections::HashMap;
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-fn get_subfolders_path(folder_path: &Path) -> Vec<String> {
-    let mut subfolders_path: Vec<String> = Vec::new();
+fn get_subfolders_path(folder_path: &Path) -> Vec<PathBuf> {
+    let mut subfolders_path: Vec<PathBuf> = Vec::new();
 
-    let walker = WalkBuilder::new(folder_path)
-        .max_depth(Some(1))
-        .git_ignore(true)
-        .build();
+    let walker = WalkBuilder::new(folder_path).git_ignore(true).build();
 
     for result in walker {
         if let Ok(entry) = result {
             let current_path = entry.path();
 
-            if current_path.is_dir() && current_path != folder_path {
-                if let Some(nom) = current_path.file_name() {
-                    if let Some(nom_str) = nom.to_str() {
-                        subfolders_path.push(nom_str.to_string());
-                    }
-                }
+            if current_path.is_dir() {
+                subfolders_path.push(current_path.to_path_buf());
             }
         }
     }
 
     subfolders_path
 }
-fn get_subfolders_name(folder_path: &Path) -> Vec<String> {
-    let mut subfolders_name: Vec<String> = Vec::new();
 
-    let walker = WalkBuilder::new(folder_path)
-        .max_depth(Some(1))
-        .git_ignore(true)
-        .build();
+fn get_subfolders_name(folders_path: Vec<PathBuf>) -> HashMap<PathBuf, Vec<String>> {
+    let mut h = HashMap::new();
 
-    for result in walker {
-        if let Ok(entry) = result {
-            let current_path = entry.path();
+    for folder in folders_path {
+        let mut subfolders_name: Vec<String> = Vec::new();
 
-            if current_path.is_dir() && current_path != folder_path {
-                if let Some(nom) = current_path.file_name() {
-                    if let Some(nom_str) = nom.to_str() {
-                        subfolders_name.push(nom_str.to_string());
+        let walker = WalkBuilder::new(&folder)
+            .max_depth(Some(1))
+            .git_ignore(true)
+            .build();
+
+        for result in walker {
+            if let Ok(entry) = result {
+                let current_path = entry.path();
+
+                if current_path.is_dir() && current_path != folder {
+                    if let Some(name) = current_path.file_name() {
+                        if let Some(name_str) = name.to_str() {
+                            subfolders_name.push(name_str.to_string());
+                        }
                     }
                 }
             }
         }
+
+        h.insert(folder, subfolders_name);
     }
 
-    subfolders_name
+    h
 }
 
 fn main() {
@@ -66,23 +67,21 @@ fn main() {
         return;
     }
 
-    // firstly, get all folders path as a Vector, e.g.
-    // ["folder-organization", "folder-organization/src", "folder-organization/tests", "folder-organization/tests/sub1"]
-
-    // secondly, get direct subfolders of each folder
-    // {
-    //  "folder-organization": ["src", "tests"],
-    //  "folder-organization/src": [],
-    //  "folder-organization/tests": ["sub1"],
-    //  "folder-organization/tests/sub1": [],
-    // }
-
-    let subfolders = get_subfolders_name(path);
+    let subfolders = get_subfolders_path(path);
     let mut result = String::new();
 
-    for folder in subfolders {
-        result.push_str(&format!("[`📂 {}`]({})\n> todo\n\n", folder, folder));
+    for folder in &subfolders {
+        result.push_str(&format!(
+            "[`📂 {}`]({})\n> todo\n\n",
+            folder.display(),
+            folder.display()
+        ));
     }
 
     println!("{}", result);
+
+    let r = get_subfolders_name(subfolders);
+
+    println!();
+    println!("{:?}", r);
 }
