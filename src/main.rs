dda0548367fbeee7c56823bd608a47d969f70612
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2024-08-17 02:00:23 +0200

    feat(wip): working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use ignore::WalkBuilder;
+use regex::Regex;
 use std::collections::HashMap;
-use std::{env, fs};
 use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 fn get_subfolders_path(folder_path: &Path) -> Vec<PathBuf> {
     let mut subfolders_path: Vec<PathBuf> = Vec::new();
@@ -52,35 +53,69 @@ fn get_subfolders_name(folders_path: Vec<PathBuf>) -> HashMap<PathBuf, Vec<Strin
     h
 }
 
+fn default_description(folder_name: &str) -> &str {
+    match folder_name {
+        "docs" => "Documentation.",
+        "src" => "Source code.",
+        "tests" => "Unit tests.",
+        _ => "todo",
+    }
+}
+
+fn markdown_content(folder: &Path, subfolders: &[String]) -> (String, String) {
+    let folder_name = folder
+        .file_name()
+        .and_then(|os_str| os_str.to_str())
+        .unwrap_or("")
+        .to_string();
+
+    let header = format!("# {} folder\n\n## Description\n\ntodo\n", folder_name);
+
+    // Définir le texte de la section "Folder Organization"
+    let mut new_section = format!(
+        "\n## Folder organization\n\n{}\n",
+        subfolders
+            .iter()
+            .map(|name| format!("[`📂 {}`]({})\n> {}", name, name, default_description(name)))
+            .collect::<Vec<_>>()
+            .join("\n\n")
+    );
+
+    if subfolders.is_empty() {
+        new_section = "".to_string();
+    }
+
+    (header, new_section)
+}
+
 fn update_readme(folder: &Path, subfolders: &[String]) -> std::io::Result<()> {
     let readme_path = folder.join("README.md");
-    let mut content = String::new();
+    let (header, new_section) = markdown_content(folder, subfolders);
 
     // Vérifier si README.md existe et le lire
     if readme_path.exists() {
+        let readme_content = fs::read_to_string(&readme_path)?;
+        // Définir une expression régulière pour capturer la section
+        let re = Regex::new(r"(?s)(## Folder organization.*?)(\n## |\z)").unwrap();
+
+        // Vérifier si la section existe
+        let updated_content = if let Some(captures) = re.captures(&readme_content) {
+            // Remplacer la section capturée par le nouveau contenu
+            let section_start = &captures[1];
+            readme_content.replacen(section_start, &new_section, 1)
+        } else {
+            // Ajouter la section à la fin si elle n'existe pas
+            format!("{}{}", readme_content, new_section)
+        };
+
+        // Écrire le contenu mis à jour dans le fichier
+        fs::write(&readme_path, updated_content)?;
+
         return Ok(());
     }
 
-    // Définir le texte de la section "Folder Organization"
-    let new_section = format!(
-        "# Folder Organization\n\n{}\n",
-        subfolders
-            .iter()
-            .map(|name| format!("- {}", name))
-            .collect::<Vec<_>>()
-            .join("\n")
-    );
-
-
-    // Sinon, ajouter la section à la fin
-    if !content.is_empty() {
-        content.push_str("\n");
-    }
-    content.push_str(&new_section);
-
-
     // Écrire les modifications dans le fichier README.md
-    fs::write(&readme_path, content)?;
+    fs::write(&readme_path, format!("{}{}", header, new_section))?;
 
     Ok(())
 }
